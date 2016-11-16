4096facb7fbd201af58f0ba0f864b6492f8dc52a
scantero <sm.cantero@gmail.com>
2016-11-16 11:49:10 +0100

    Añadidas nuevas tablas al sql

--- tutorial.sql
+++ tutorial.sql
@@ -30,3 +30,25 @@ CREATE TABLE towns (
 
 PARTITION TABLE towns ON COLUMN state_num;
 PARTITION TABLE people ON COLUMN state_num;
+
+CREATE TABLE nws_event (
+   id VARCHAR(256) NOT NULL,
+   type VARCHAR(128),
+   severity VARCHAR(128),
+   SUMMARY VARCHAR(1024),
+   starttime TIMESTAMP,
+   endtime TIMESTAMP,
+   updated TIMESTAMP,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE local_event (
+    state_num TINYINT NOT NULL,
+    county_num SMALLINT NOT NULL,
+    id VARCHAR(256) NOT NULL
+);
+
+CREATE INDEX local_event_idx ON local_event (state_num, county_num);
+CREATE INDEX nws_event_idx ON nws_event (id);
+
+PARTITION TABLE local_event ON COLUMN state_num;
